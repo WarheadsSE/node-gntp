841fdcbe86d32cf48046893d4544d6d47c92dac6
Jason Plum <max@warheads.net>
2011-11-08 14:05:15 -0500

    Replace GNTP.js w/ v0.6.0 compat, and update README

---
+++ gntp-send.js
@@ -0,0 +1,56 @@
+
+
+var fs = require('fs');
+var gntp = require('GNTP');
+
+var icon = fs.readFileSync('./nodejs.jpg');
+var warn_icon = fs.readFileSync('./warning_icon.jpg');
+
+var gntpResponse = function (response){
+        console.log('Response Recieved');
+        console.log('Type:', response.type);
+        if( response.type === gntp.MessageTypeEnum.OK || response.type === gntp.MessageTypeEnum.ERROR ){
+            console.log('From:', response.headers.getHeader(gntp.HeaderEnum.responseAction).value);
+        }
+        var headers = response.headers.headers;
+        for( var i=0; i<headers.length; i++){
+            console.log(headers[i].name+':='+headers[i].value);
+        }
+    };
+
+var client = new gntp.Client();
+client.host = '192.168.11.6';
+
+client.on('sent',function () { console.log('sent');} );
+client.on('response',function (msg) { console.log('Response:  '+msg.type);});
+
+var app = new gntp.Application('Node.js');
+app.icon = icon;
+
+var notify = new gntp.Notification();
+notify.name = 'Test';
+notify.displayName = 'Node.js Test';
+notify.enabled = true;
+
+var appReq = app.toRequest();
+appReq.addNotification(notify);
+
+client.sendMessage(appReq.toRequest());
+
+var notReq = notify.toRequest();
+notReq.applicationName = app.name;
+notReq.text = 'testing Node.js';
+notReq.icon = warn_icon;
+
+var msg = notReq.toRequest();
+msg.headers.addHeader(new gntp.Header(gntp.HeaderEnum.dataHeaderPrefix+'Blarg','blarg'));
+
+msg.crypto = new gntp.Crypto('nodejs','sha1');//,'des');
+
+
+setTimeout(function () {
+    console.log("sending...");
+    client.sendMessage(msg,gntpResponse);
+    console.log("sent...?");
+},3000);
+
