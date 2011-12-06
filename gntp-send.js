b2360a5cdf3fc3ebe9050dc545d5c29b3d4f52e1
Jason Plum <max@warheads.net>
2011-12-06 12:44:38 -0500

    New: - including icons. Functional: - receiving & responding to standard REGISTER/NOTIFY - Encryption in both directions (although responses are not encrypted atm in server.js) - Handling of segmented messages. >.< Not Yet: - receiving/responding to SUBSCRIBE - Server class incomplete.
    Signed-off-by: unknown <Jason@.(none)>

--- gntp-send.js
+++ gntp-send.js
@@ -19,7 +19,7 @@ var gntpResponse = function (response){
     };
 
 var client = new gntp.Client();
-client.host = '192.168.11.6';
+client.host = '192.168.11.7';
 
 client.on('sent',function () { console.log('sent');} );
 client.on('response',function (msg) { console.log('Response:  '+msg.type);});
@@ -34,8 +34,8 @@ notify.enabled = true;
 
 var appReq = app.toRequest();
 appReq.addNotification(notify);
-
-client.sendMessage(appReq.toRequest());
+var aRmime = appReq.toRequest();
+client.sendMessage(aRmime);
 
 var notReq = notify.toRequest();
 notReq.applicationName = app.name;
@@ -45,12 +45,15 @@ notReq.icon = warn_icon;
 var msg = notReq.toRequest();
 msg.headers.addHeader(new gntp.Header(gntp.HeaderEnum.dataHeaderPrefix+'Blarg','blarg'));
 
-msg.crypto = new gntp.Crypto('nodejs','sha1');//,'des');
+msg.crypto = new gntp.Crypto('nodejs','sha1','des');
 
 
-setTimeout(function () {
+client.on('response',gntpResponse)
+
+setInterval(function () {
+//setTimeout(function () {
     console.log("sending...");
-    client.sendMessage(msg,gntpResponse);
+    client.sendMessage(msg);
     console.log("sent...?");
-},3000);
+},1000);
 
--- server.js
+++ server.js
@@ -2,69 +2,77 @@
 var net = require('net');
 var fs = require('fs');
 var gntp = require('GNTP');
-var rbytes = require('rbytes');
 
-function findMessageEnd (buff){
-    if( ( buff instanceof Buffer ) && buff.length >= 6){
-        for( var i=0; i < buff.length -6; i++ ){
-            if( buff[i+0] == 13 && buff[i+1] == 10 &&
-                buff[i+2] == 13 && buff[i+3] == 10 &&
-                buff[i+4] == 13 && buff[i+5] == 10){
-                //End!
-                return i;
-            }
-        }
+var respond = function (request,sock){
+    var response = new gntp.Message();
+
+    for(var m=0; m<request.headers.pointerHeaders.length; m++){
+        console.log(
+            'PH=>'
+            + request.headers.pointerHeaders[m].name
+            + ':'
+            + request.headers.pointerHeaders[m].value
+            + ' ['
+            + request.headers.pointerHeaders[m].resourceData.length
+            + ']');
     }
-     return -1;
-}
-
-
+    response.type = gntp.MessageTypeEnum.OK;
+    response.headers.addHeader(new gntp.Header(gntp.HeaderEnum.responseAction,request.type));
+    
+    // return any Data headers.
+    for(var n=0; n<request.headers.dataHeaders.length; n++){
+        response.headers.addHeader(request.headers.dataHeaders[n]);
+    }
+    
+    var protocol = response.protocolString();
+    var mime = response.mime();
+    //console.log('response:'+protocol+mime.toString('utf8'));
+    //fs.writeFileSync('encrypt.log',mime);
+    var mBuff = new Buffer(protocol.length+mime.length+(gntp.EndLine.length));
+    mBuff.write(protocol,0,'utf8');
+    mime.copy(mBuff,protocol.length,0);
+    mBuff.write(gntp.EndLine,protocol.length+mime.length,'utf8');
+    
+    sock.write(mBuff);
+};
+var connections = 0;
 var server = net.createServer();
 server.on('connection',function (sock){
+    var msg = new gntp.Message();
     var recvd = 0,
-        chunkSize = 1024,
-        eomLength = 6,
         parsed = false;
-    var msg = new Buffer(chunkSize);
-    
+    var id = connections++;
     sock.on('data',function (data){
-        var tmp, eol;
-        if( data.length + recvd > msg.length){
-            tmp = new Buffer(msg.length+chunkSize);
-            msg.copy(tmp);
-            msg = tmp;
+        if( parsed === true) {
+            recvd = 0;
+            msg = new gntp.Message();
         }
+        console.log('SERVER RECV:'+data.length+' on '+id);
         
-        data.copy(msg,recvd);
-        console.log('SERVER RECV:'+data.length);
-        eom = findMessageEnd(msg.slice((recvd >=eomLength?recvd-eomLength:recvd)));
+        eom = msg.parse(data);
         
         recvd+= data.length;
-        
+        /*
         var logFileU = fs.open('recv.log','a',null, function (err,fd){
             fs.writeSync(fd,data,0,data.length,recvd);
         });
-        if( eom >= 0 ){
-            var message = new gntp.Message();
-            message.parse(msg);
+        */
+        if( eom === true ){
             parsed = true;
+            respond(msg,sock);
             sock.end();
         }
     });
     sock.on('close',function (){
-        console.log('socket closed');
-        console.log('RCVD:'+recvd);
-        // if the message wasn't parsed, attempt to parse it now.
-        if( !parsed ){
-            var message = new gntp.Message();
-            message.parse(msg);
-            parsed = true;
-        }
+        //console.log('socket closed '+id);
+        console.log('RCVD:'+recvd+' on '+id);
         console.log('good:'+(parsed?'yes':'no'));
+        console.log('===');
     });
     setTimeout( function (){
         sock.end();
     },3000);
 });
 server.listen(23053);
+console.log('Listening on GNTP service port (23053)');
 
